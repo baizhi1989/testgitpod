09db8ee028107b0e7f20ee92e22e90424c68a886
baizhi1989 <>
2025-06-25 23:12:23 +0800

    Last Sync: 2025-06-25 15:12:23 (Mobile)

--- query.sh
+++ query.sh
@@ -8,8 +8,6 @@
 
 # curl -o /home/user/b.qcow2 https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2
 
-wget -O /home/user/ubuntu.iso http://de.releases.ubuntu.com/16.04/ubuntu-16.04.3-server-amd64.iso
-
-qemu-img create -f qcow2 /home/user/ubuntu.qcow2 30G
-
-qemu-system-x86_64 -cpu host -enable-kvm -m 512 -hda /home/user/ubuntu.qcow2 -cdrom /home/user/ubuntu.iso -usbdevice tablet
+wget -O /home/user/ubuntu.iso http://de.releases.ubuntu.com/16.04/ubuntu-16.04.3-server-amd64.iso \
+&& qemu-img create -f qcow2 /home/user/ubuntu.qcow2 30G \
+&& qemu-system-x86_64 -cpu host -enable-kvm -m 512 -hda /home/user/ubuntu.qcow2 -cdrom /home/user/ubuntu.iso -usbdevice tablet
