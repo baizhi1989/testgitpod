54b4cf662c6185b9ac86dcc16cf02decdb8f3e06
baizhi1989 <>
2025-06-25 21:54:49 +0800

    Last Sync: 2025-06-25 13:54:49 (Mobile)

--- query.sh
+++ query.sh
@@ -7,9 +7,11 @@ if ! dpkg -s curl &> /dev/null; then
 echo "curl未安装，开始安装..."
 # 更新包索引并安装curl（-y自动确认）
 sudo apt update && sudo apt install -y curl
+fi
 # 检查安装是否成功
 if [ $? -eq 0 ]; then
 echo "curl安装成功！"
 else
 echo "curl安装失败，请检查网络或权限。"
 fi
+
